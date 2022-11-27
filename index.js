ddc2b97bedc68eb2ab426d1c64e39d6a95af66e4
Solves <solvesweb@gmail.com>
2022-11-27 17:18:46 +0100

    Commit para la primera versión

--- index.js
+++ index.js
@@ -1,10 +1,18 @@
+// Declaración de variables
 const burguerIcon = document.querySelector("#burguer-menu");
 const mobileMenu = document.querySelector(".mobile-menu");
 const section = document.querySelector("section");
+const footer = document.querySelector("footer");
 
+
+// LLamadas a las funciones
 burguerIcon.addEventListener("click", togleMobileMenu);
-/* bodyDiv.addEventListener("click", closeMobileMenu); */
+mobileMenu.addEventListener("click", togleMobileMenu);
+section.addEventListener("click", togleMobileMenu);
+footer.addEventListener("click", togleMobileMenu);
 
+
+// Declaración de funciones
 function togleMobileMenu() {
   mobileMenu.classList.toggle("inactive");
 };
